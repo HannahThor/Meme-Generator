ec2ddde9a7d61bfd7b7292cf98d201f22be101d2
Hannah Thorley <hannahthorley2014@gmail.com>
2024-04-04 22:44:10 +0100

    testing colours

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ export default function Header() {
     <header className="header">
       <img src="./public/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
