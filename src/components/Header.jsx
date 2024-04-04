c4c1ce3b062b9f75cdce1407c49305aed9fb3b05
Hannah Thorley <hannahthorley2014@gmail.com>
2024-04-04 23:53:54 +0100

    remove smiley

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="./public/troll-face.png" className="header--image" />
+      {/* <img src="./public/troll-face.png" className="header--image" /> */}
       <h2 className="header--title">Meme Generator</h2>
     </header>
   );
