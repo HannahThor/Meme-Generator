6f7a513f23fe03cf01d561ec47debfbe56d8e5dd
Hannah Thorley <hannahthorley2014@gmail.com>
2024-04-04 22:03:38 +0100

    unstyled Meme Generator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
